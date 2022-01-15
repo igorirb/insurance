d0cecc49a2951ae4e45cf1d81964df6d88fc2540
Igor Barroso <igor.irb@gmail.com>
2022-01-15 20:15:24 -0300

    feat(tests) testing function calls

--- tests/utils/insurance/get-risk-profile.spec.js
+++ tests/utils/insurance/get-risk-profile.spec.js
@@ -1,7 +1,8 @@
 const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
-const utils = require('../../../src/utils/insurance');
+
+const { insurance, ruler } = require('../../../src/utils');
 
 chai.use(sinonChai);
 
@@ -28,15 +29,31 @@ describe('src/utils/insurance', () => {
 
   describe('getRiskProfile()', () => {
     it('should process risk profile', () => {
-      const method = sinon.spy(utils, 'processRiskProfile');
-      const result = utils.getRiskProfile(input);
-      expect(method).to.have.been.called;
+      const processRiskProfile = sinon.spy(insurance, 'processRiskProfile');
+      const applyAgeRules = sinon.spy(ruler, 'applyAgeRules');
+      const applyIncomeRules = sinon.spy(ruler, 'applyIncomeRules');
+      const applyIneligibilityRules = sinon.spy(ruler, 'applyIneligibilityRules');
+      const applyHouseRules = sinon.spy(ruler, 'applyHouseRules');
+      const applyDependentsRules = sinon.spy(ruler, 'applyDependentsRules');
+      const applyMaritalStatusRules = sinon.spy(ruler, 'applyMaritalStatusRules');
+      const applyVehicleRules = sinon.spy(ruler, 'applyVehicleRules');
+
+      const result = insurance.getRiskProfile(input);
       expect(result).to.be.deep.equal({
         auto: 'regular',
         disability: 'ineligible',
         home: 'economic',
         life: 'regular',
       });
+
+      expect(processRiskProfile.called).to.be.equal(true);
+      expect(applyAgeRules.called).to.be.equal(true);
+      expect(applyIncomeRules.called).to.be.equal(true);
+      expect(applyIneligibilityRules.called).to.be.equal(true);
+      expect(applyHouseRules.called).to.be.equal(true);
+      expect(applyDependentsRules.called).to.be.equal(true);
+      expect(applyMaritalStatusRules.called).to.be.equal(true);
+      expect(applyVehicleRules.called).to.be.equal(true);
     });
   });
 });
